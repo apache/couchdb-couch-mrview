d685f9abcf9f75e9d4ee83d9b0848ccf086bf32e
Mayya Sharipova <mayyas@ca.ibm.com>
2016-08-15 09:26:48 -0400

    Update couch_mrview for new purge API
    Add test for view after purge requests
    
    BugzID: 68276

---
+++ test/couch_mrview_purge_docs_tests.erl
@@ -0,0 +1,123 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-module(couch_mrview_purge_docs_tests).
+
+-include_lib("couch/include/couch_eunit.hrl").
+-include_lib("couch/include/couch_db.hrl").
+
+-define(TIMEOUT, 1000).
+
+
+setup() ->
+    {ok, Db} = couch_mrview_test_util:init_db(?tempdb(), map, 5),
+    Db.
+
+teardown(Db) ->
+    couch_db:close(Db),
+    couch_server:delete(Db#db.name, [?ADMIN_CTX]),
+    ok.
+
+view_purge_test_() ->
+    {
+        "Map views",
+        {
+            setup,
+            fun test_util:start_couch/0, fun test_util:stop_couch/1,
+            {
+                foreach,
+                fun setup/0, fun teardown/1,
+                [
+                    fun test_purge_single/1,
+                    fun test_purge_multiple/1
+                ]
+            }
+        }
+    }.
+
+
+test_purge_single(Db) ->
+    Result = run_query(Db, []),
+    Expect = {ok, [
+        {meta, [{total, 5}, {offset, 0}]},
+        {row, [{id, <<"1">>}, {key, 1}, {value, 1}]},
+        {row, [{id, <<"2">>}, {key, 2}, {value, 2}]},
+        {row, [{id, <<"3">>}, {key, 3}, {value, 3}]},
+        {row, [{id, <<"4">>}, {key, 4}, {value, 4}]},
+        {row, [{id, <<"5">>}, {key, 5}, {value, 5}]}
+    ]},
+    ?_assertEqual(Expect, Result),
+
+    FDI = couch_db:get_full_doc_info(Db, <<"1">>),
+    Rev = get_rev(FDI),
+    {ok, _, _} = couch_db:purge_docs(Db, {<<"1">>, [Rev]}),
+    {ok, Db2} = couch_db:reopen(Db),
+
+    Result2 = run_query(Db2, []),
+    Expect2 = {ok, [
+        {meta, [{total, 4}, {offset, 0}]},
+        {row, [{id, <<"2">>}, {key, 2}, {value, 2}]},
+        {row, [{id, <<"3">>}, {key, 3}, {value, 3}]},
+        {row, [{id, <<"4">>}, {key, 4}, {value, 4}]},
+        {row, [{id, <<"5">>}, {key, 5}, {value, 5}]}
+    ]},
+    ?_assertEqual(Expect2, Result2).
+
+
+test_purge_multiple(Db) ->
+    Result = run_query(Db, []),
+    Expect = {ok, [
+        {meta, [{total, 5}, {offset, 0}]},
+        {row, [{id, <<"1">>}, {key, 1}, {value, 1}]},
+        {row, [{id, <<"2">>}, {key, 2}, {value, 2}]},
+        {row, [{id, <<"3">>}, {key, 3}, {value, 3}]},
+        {row, [{id, <<"4">>}, {key, 4}, {value, 4}]},
+        {row, [{id, <<"5">>}, {key, 5}, {value, 5}]}
+    ]},
+    ?_assertEqual(Expect, Result),
+
+    % 1st purge request
+    FDI1 = couch_db:get_full_doc_info(Db, <<"1">>),
+    Rev1 = get_rev(FDI1),
+    {ok, _, _} = couch_db:purge_docs(Db, {<<"1">>, [Rev1]}),
+    {ok, Db2} = couch_db:reopen(Db),
+
+    % 2nd purge request
+    FDI2 = couch_db:get_full_doc_info(Db, <<"2">>),
+    Rev2 = get_rev(FDI2),
+    {ok, _, _} = couch_db:purge_docs(Db2, {<<"2">>, [Rev2]}),
+    {ok, Db3} = couch_db:reopen(Db2),
+
+    % 3rd purge request
+    FDI3 = couch_db:get_full_doc_info(Db, <<"5">>),
+    Rev3 = get_rev(FDI3),
+    {ok, _, _} = couch_db:purge_docs(Db3, {<<"5">>, [Rev3]}),
+    {ok, Db4} = couch_db:reopen(Db3),
+
+    Result2 = run_query(Db4, []),
+    Expect2 = {ok, [
+        {meta, [{total, 2}, {offset, 0}]},
+        {row, [{id, <<"3">>}, {key, 3}, {value, 3}]},
+        {row, [{id, <<"4">>}, {key, 4}, {value, 4}]}
+    ]},
+    ?_assertEqual(Expect2, Result2).
+
+
+run_query(Db, Opts) ->
+    couch_mrview:query_view(Db, <<"_design/bar">>, <<"baz">>, Opts).
+
+
+get_rev(#full_doc_info{} = FDI) ->
+    #doc_info{
+        revs = [#rev_info{} = PrevRev | _]
+    } = couch_doc:to_doc_info(FDI),
+    PrevRev#rev_info.rev.
